6fe7c1d92e74920255c5fbd11d8984dca57841fd
helpfulrobot <chris@silverstripe.com>
2016-01-01 05:15:38 +1300

    Converted to PSR-2

--- code/HiddenFieldsDataExtension.php
+++ code/HiddenFieldsDataExtension.php
@@ -1,18 +1,22 @@
 <?php
-class HiddenFieldsDataExtension extends DataExtension {
+class HiddenFieldsDataExtension extends DataExtension
+{
 
-	public function getCMSFields() {
- 		$fields = parent::getCMSFields();
-		$this->extend('updateCMSFields', $fields);
-	    return $fields;
-	}
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+        $this->extend('updateCMSFields', $fields);
+        return $fields;
+    }
 
-	public function updateCMSFields(FieldList $fields) {
- 		$owner = $this->owner;
- 		if ( isset($owner::$hidden_fields)) foreach ($owner::$hidden_fields as $field) {
-			$fields->removeByName($field);
-			$fields->push(new HiddenField($field,false ));
-
- 		}
-	}
+    public function updateCMSFields(FieldList $fields)
+    {
+        $owner = $this->owner;
+        if (isset($owner::$hidden_fields)) {
+            foreach ($owner::$hidden_fields as $field) {
+                $fields->removeByName($field);
+                $fields->push(new HiddenField($field, false));
+            }
+        }
+    }
 }
